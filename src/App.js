df38d9c646856d0f674a02aefdc0b82fff2829ba
neha150999 <neha150saxena.com>
2024-12-23 16:46:31 -0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-800">
+     Namate Everyone! Lets build netflix GPT
+    </div>
+  );
+}
+
+export default App;
