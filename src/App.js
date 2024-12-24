37dd5f7fbd1907f6c2b9dbf80c30a257dedc6078
neha150999 <neha150saxena.com>
2024-12-23 17:15:13 -0800

    third commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="text-3xl font-bold text-green-800">
      Namate Everyone! Lets build netflix GPT. Hope you are doing well.
+     testing git
     </div>
   );
 }
